0da19885c6bba7a43a01137507aa603531a7ef96
Donte' Williams <50161318+dinoman20@users.noreply.github.com>
2023-11-09 17:01:00 -0800

    Delete Phase2-final directory

--- Phase2-final/README.md
+++
@@ -1,4 +0,0 @@
-# CSE111--Project
-CSE111 project
-
-Video_game Store
--- Phase2-final/empty-tables.sql
+++
@@ -1,7 +0,0 @@
-delete from Game;
-delete from Platform;
-delete from Store;
-delete from Customer;
-delete from employee;
-delete from Publisher;
-delete from Developer;
